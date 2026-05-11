cc8521501ed4af0868ab66b9da68a44ac1774a3c
msvstj <savastijonok@gmail.com>
2026-05-11 15:57:36 +0300

    feat(network-config): add ethereal network configuration

---
+++ apps/web/src/config/templateConfig.ts
@@ -0,0 +1,22 @@
+import type { TemplateConfig } from './constants.extra'
+
+/**
+ * THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
+ * Generated from /networks/ethereal/config.json
+ */
+const TEMPLATE_CONFIG = {
+  EIP155: false,
+  SUPPORTED_VERSIONS: ['1.3.0', '1.4.1'],
+  SAFE_UTILS_SUPPORTED: false,
+  IS_LICENSED: false,
+  LOGO_DIMENSIONS: {
+    HEADER: {
+      H: '40px',
+    },
+    WELCOME: {
+      W: '200px',
+      H: '40px',
+    },
+  },
+} as TemplateConfig
+export default TEMPLATE_CONFIG
